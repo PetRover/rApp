564bb7ba46cf59f4d69170701ec122c37ee35e74
Bryce Cater <brycedcarter@gmail.com>
2015-11-23 17:26:29 -0500

    jpeg transfer and display working

--- rApp.cpp
+++ rApp.cpp
@@ -96,8 +96,8 @@ rAppMainView::rAppMainView(QWidget *parent)
 
 void rAppMainView::listen()
 {
-    this->networkManager->initializeNewConnection("COMMANDS", "192.168.1.3", "192.168.1.6", 1024, RVR::ConnectionInitType::LISTEN, RVR::ConnectionProtocol::TCP);
-    this->networkManager->initializeNewConnection("CAMERA", "192.168.1.3", "192.168.1.6", 1025, RVR::ConnectionInitType::LISTEN, RVR::ConnectionProtocol::UDP);
+    this->networkManager->initializeNewConnection("COMMANDS", "192.168.1.3", "192.168.1.5", 1024, RVR::ConnectionInitType::LISTEN, RVR::ConnectionProtocol::TCP);
+    this->networkManager->initializeNewConnection("CAMERA", "192.168.1.3", "192.168.1.5", 1025, RVR::ConnectionInitType::LISTEN, RVR::ConnectionProtocol::UDP);
 
 }
 
@@ -159,27 +159,7 @@ void rAppMainView::startStream()
     cmd.setCommandType(RVR::CommandType::START_STREAM);
     RVR::NetworkChunk nc = cmd.toNetworkChunk();
     this->networkManager->sendData("COMMANDS", &nc);
-    this->frameTimer->start(10);
-    usleep(100000);
-    this->stopDriving();
-    usleep(100000);
-    this->stopDriving();
-    usleep(100000);
-    this->stopDriving();
-//    VLOG(2) << "Setting stream at YUYV, 640px by 480px @ 30fps";
-//    this->camera->setupStream(UVC_FRAME_FORMAT_YUYV, 640, 480, 30);
-//    VLOG(2) << "Setting callback function to saveFrame()";
-//    this->camera->setFrameCallback(RVR::queueFrame);
-//    this->camera->setAutoExposure(true);
-//
-//
-//    this->camera->startStream();
-
-//    VLOG(2) << "Issuing START_STREAM command";
-//    RVR::Command cmd = RVR::Command();
-//    cmd.setCommandType(RVR::CommandType::START_STREAM);
-//    RVR::NetworkChunk nc = cmd.toNetworkChunk();
-//    this->networkManager->sendData("COMMANDS", &nc);
+    this->frameTimer->start(0);
 }
 
 void rAppMainView::flipCamera()
@@ -199,60 +179,14 @@ void rAppMainView::getFrames()
     {
         if (this->currentCamChunk->getDataType() == RVR::DataType::CAMERA)
         {
-            unsigned char* yuvdata = (unsigned char*)this->currentCamChunk->getData();
 
-            SwsContext *ConversionContext;
-            ConversionContext = sws_getCachedContext(NULL, 640, 480, AV_PIX_FMT_YUYV422 , 640, 480, AV_PIX_FMT_RGB24, SWS_BILINEAR, 0, 0, 0);
-
-            AVFrame* yuvFrame = av_frame_alloc();
-            AVFrame* rgbFrame = av_frame_alloc();
-
-            int num_bytes = avpicture_get_size(AV_PIX_FMT_RGB24, 640, 480);
-            uint8_t* rgbFrame_buffer = (uint8_t *)av_malloc(num_bytes*sizeof(uint8_t));
-
-            avpicture_fill((AVPicture*)rgbFrame, rgbFrame_buffer, AV_PIX_FMT_RGB24, 640, 480);
-            avpicture_fill((AVPicture*)yuvFrame, yuvdata, AV_PIX_FMT_YUYV422, 640, 480);
-
-            sws_scale(ConversionContext, (const uint8_t* const*)yuvFrame->data, yuvFrame->linesize, 0, 480, rgbFrame->data, rgbFrame->linesize);
-
-            for( int y = 0; y < 480; ++y )
-            {
-                memcpy( this->cameraImage.scanLine(y), rgbFrame->data[0]+y * rgbFrame->linesize[0],  rgbFrame->linesize[0]  );
-            }
-
+            unsigned char* jpegData = (unsigned char*)this->currentCamChunk->getData();
+            this->cameraImage.loadFromData(jpegData, this->currentCamChunk->getLength(), "JPEG");
             myLabel->setPixmap(QPixmap::fromImage(this->cameraImage));
         }
     }
     VLOG(3) << "[DONE] getting frames ";
 
-//    if (!this->camera->frameQueue.empty())
-//    {
-//        VLOG(3) << "Got frame from a queue with length: " << this->camera->frameQueue.size();
-//        RVR::NetworkChunk* nc = this->camera->frameQueue.front();
-//        this->camera->frameQueue.pop();
-//        unsigned char* yuvdata = (unsigned char*)nc->getData();
-//
-//
-//        SwsContext *ConversionContext;
-//        ConversionContext = sws_getCachedContext(NULL, 640, 480, AV_PIX_FMT_YUYV422 , 640, 480, AV_PIX_FMT_RGB24, SWS_BILINEAR, 0, 0, 0);
-//
-//        AVFrame* yuvFrame = av_frame_alloc();
-//        AVFrame* rgbFrame = av_frame_alloc();
-//
-//        int num_bytes = avpicture_get_size(AV_PIX_FMT_RGB24, 640, 480);
-//        uint8_t* rgbFrame_buffer = (uint8_t *)av_malloc(num_bytes*sizeof(uint8_t));
-//
-//        avpicture_fill((AVPicture*)rgbFrame, rgbFrame_buffer, AV_PIX_FMT_RGB24, 640, 480);
-//        avpicture_fill((AVPicture*)yuvFrame, yuvdata, AV_PIX_FMT_YUYV422, 640, 480);
-//
-//        sws_scale(ConversionContext, (const uint8_t* const*)yuvFrame->data, yuvFrame->linesize, 0, 480, rgbFrame->data, rgbFrame->linesize);
-//
-//        QImage img = QImage( 640, 480, QImage::Format_RGB888);
-//        for( int y = 0; y < 480; ++y )
-//        {
-//            memcpy( img.scanLine(y), rgbFrame->data[0]+y * rgbFrame->linesize[0],  rgbFrame->linesize[0]  );
-//        }
-//        myLabel->setPixmap(QPixmap::fromImage(img));
-////        this->canvas->setImage(img);
+
 //    }
 }
